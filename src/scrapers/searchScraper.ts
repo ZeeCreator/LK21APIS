b0cfa5e810751a996e77b08e6734e62b6c5274b9
Era Corp <zero@gmail.com>
2026-05-29 09:37:45 +0700

    fix search: add post_type[]=post&post_type[]=tv filter

--- src/scrapers/searchScraper.ts
+++ src/scrapers/searchScraper.ts
@@ -6,7 +6,7 @@ export class SearchScraper extends BaseScraper {
   async scrape(query: string, page: number = 1): Promise<ParsedSearchResult> {
     const encoded = encodeURIComponent(query);
     const params = page > 1 ? `&page=${page}` : '';
-    const url = `/?s=${encoded}${params}`;
+    const url = `/?s=${encoded}${params}&post_type[]=post&post_type[]=tv`;
     logger.info({ url, query }, 'Scraping search results');
     const html = await this.fetchWithRetry(url);
     return parseSearchResults(html, query);
